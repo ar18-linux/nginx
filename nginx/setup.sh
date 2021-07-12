d5e23cc65b75aad34db516bf81a75a3248e20164
nulysses <adr.zivil@gmail.com>
2021-07-12 22:30:31 +0200

    1

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -189,7 +189,7 @@ ar18.script.import ar18.script.read_target
 
 ar18.script.obtain_sudo_password
 
-ar18.script.execute_with_sudo sed -i -E "s^http {^http {\n    include /etc/nginx/sites-enabled/*;'^g" "/etc/nginx/nginx.conf"
+ar18.script.execute_with_sudo sed -i -E "s^http \{^http \{\n    include /etc/nginx/sites-enabled/*;'^g" "/etc/nginx/nginx.conf"
 
 ar18.script.execute_with_sudo mkdir -p "/etc/nginx/sites-avaialable"
 ar18.script.execute_with_sudo mkdir -p "/etc/nginx/sites-enabled"
