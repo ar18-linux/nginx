0ade2b176403aba692c22f31a792a9654b5acfcd
nulysses <adr.zivil@gmail.com>
2021-07-12 23:31:50 +0200

    1

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -210,10 +210,10 @@ ar18.script.execute_with_sudo cp -f "${script_dir}/config/${server_name}.conf" "
 ar18.script.execute_with_sudo ln -s "/etc/nginx/sites-available/${server_name}" "/etc/nginx/sites-enabled/${server_name}"
 
 ar18.script.execute_with_sudo rm -rf "/var/www/${server_name}"
-ar18.script.execute_with_sudo mkdir -p "/var/www/${server_name}/html/foo"
+ar18.script.execute_with_sudo mkdir -p "/var/www/${server_name}/html/foo/dir"
 
 ar18.script.execute_with_sudo bash -c "echo \"hello\" > \"/var/www/${server_name}/html/index.html\"" 
-ar18.script.execute_with_sudo bash -c "echo \"hello again\" > \"/var/www/${server_name}/html/foo/index.html\"" 
+ar18.script.execute_with_sudo bash -c "echo \"hello again\" > \"/var/www/${server_name}/html/foo/dir/index.html\"" 
 
 ar18.script.execute_with_sudo systemctl start nginx
 
