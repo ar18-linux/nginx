de624955c1a36b13366b0c4ede276db36a97b004
nulysses <adr.zivil@gmail.com>
2021-07-12 22:18:51 +0200

    1

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -182,9 +182,12 @@ trap 'err_report "${BASH_SOURCE[0]}" ${LINENO} "${BASH_COMMAND}"' ERR
 }
 #################################SCRIPT_START##################################
 
+ar18.script.import ar18.script.obtain_sudo_password
 ar18.script.import ar18.pacman.install
 ar18.script.import ar18.script.execute_with_sudo
 ar18.script.import ar18.script.read_target
+
+ar18.script.obtain_sudo_password
 
 server_name="test_server"
 
